e783d675edda5aa2f753b099165f52c6d2db7f45
vsv <sergey.vostryakov@iset-soft.ru>
2024-03-06 17:58:53 +0500

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from dataclasses import dataclass
 import logging
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from websocket.socketManager import WebSocketManager
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.testclient import TestClient
+from src.websocket.websocket_manager import WebSocketManager
 import json
 import argparse
+from pydantic import BaseModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", default=8000, type=int)
@@ -25,38 +30,69 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-socket_manager = WebSocketManager()
+socket_manager: WebSocketManager = WebSocketManager()
+
+class Message(BaseModel):
+    user_id: int
+    chanel_id: str
+    message: str
 
 
-@app.websocket("/api/v1/ws/{room_id}/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: int):
-    await socket_manager.add_user_to_room(room_id, websocket)
-    message = {
-        "user_id": user_id,
-        "room_id": room_id,
-        "message": f"User {user_id} connected to room - {room_id}"
-    }
-    await socket_manager.broadcast_to_room(room_id, json.dumps(message))
+@app.websocket("/ws/{chanel_id}/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, chanel_id: str, user_id: int):
+    await socket_manager.add_user_to_chanel(chanel_id, websocket)
+    message = Message(
+        user_id=user_id,
+        chanel_id=chanel_id,
+        message=f"User {user_id} connected to chanel - {chanel_id}"
+    )
+    
+    await socket_manager.broadcast_to_chanel(chanel_id, message.model_dump_json())
     try:
         while True:
             data = await websocket.receive_text()
-            message = {
-                "user_id": user_id,
-                "room_id": room_id,
-                "message": data
-            }
-            await socket_manager.broadcast_to_room(room_id, json.dumps(message))
+            message = Message(
+                user_id=user_id,
+                chanel_id=chanel_id,
+                message=data
+            )
+           
+            await socket_manager.broadcast_to_chanel(chanel_id, message.model_dump_json())
 
     except WebSocketDisconnect:
-        await socket_manager.remove_user_from_room(room_id, websocket)
+        await socket_manager.remove_user_from_chanel(chanel_id, websocket)
+        message = Message(
+            user_id=user_id,
+            chanel_id=chanel_id,
+            message=f"User {user_id} disconnected from chanel - {chanel_id}"
+        )
+        
+        await socket_manager.broadcast_to_chanel(chanel_id, message.model_dump_json())
 
-        message = {
-            "user_id": user_id,
-            "room_id": room_id,
-            "message": f"User {user_id} disconnected from room - {room_id}"
-        }
-        await socket_manager.broadcast_to_room(room_id, json.dumps(message))
 
+templates = Jinja2Templates(directory="src/templates")
+
+@app.get("/", response_class=HTMLResponse)
+def read_index(request: Request):
+    # Render the HTML template
+    return templates.TemplateResponse("index.html", {"request" : request})
+
+
+def test_read_main():
+    client = TestClient(app)
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
+
+
+def test_websocket():
+    client = TestClient(app)
+    with client.websocket_connect("/ws/1/1") as websocket:
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello WebSocket"}
+        
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=args.port, reload=True)
+    test_read_main()
+    test_websocket()
---
+++ src/websocket/websocket_manager.py
@@ -0,0 +1,78 @@
+import asyncio
+
+import json
+from fastapi import WebSocket
+from src.redis.redis_pubsub_manager import RedisPubSubManager
+
+
+class WebSocketManager:
+
+    def __init__(self):
+        """
+        Initializes the WebSocketManager.
+
+        Attributes:
+            chanels (dict): A dictionary to store WebSocket connections in different chanels.
+            pubsub_client (RedisPubSubManager): An instance of the RedisPubSubManager class for pub-sub functionality.
+        """
+        self.chanels: dict[str,list[WebSocket]] = {}
+        self.pubsub_client = RedisPubSubManager()
+
+    async def add_user_to_chanel(self, chanel_id: str, websocket: WebSocket) -> None:
+        """
+        Adds a user's WebSocket connection to a chanel.
+
+        Args:
+            chanel_id (str): chanel ID or channel name.
+            websocket (WebSocket): WebSocket connection object.
+        """
+        await websocket.accept()
+
+        if chanel_id in self.chanels:
+            self.chanels[chanel_id].append(websocket)
+        else:
+            self.chanels[chanel_id] = [websocket]
+
+            await self.pubsub_client.connect()
+            pubsub_subscriber = await self.pubsub_client.subscribe(chanel_id)
+            asyncio.create_task(self._pubsub_data_reader(pubsub_subscriber))
+
+    async def broadcast_to_chanel(self, chanel_id: str, message: str) -> None:
+        """
+        Broadcasts a message to all connected WebSockets in a chanel.
+
+        Args:
+            chanel_id (str): chanel ID or channel name.
+            message (str): Message to be broadcasted.
+        """
+        await self.pubsub_client._publish(chanel_id, message)
+
+    async def remove_user_from_chanel(self, chanel_id: str, websocket: WebSocket) -> None:
+        """
+        Removes a user's WebSocket connection from a chanel.
+
+        Args:
+            chanel_id (str): chanel ID or channel name.
+            websocket (WebSocket): WebSocket connection object.
+        """
+        self.chanels[chanel_id].remove(websocket)
+
+        if len(self.chanels[chanel_id]) == 0:
+            del self.chanels[chanel_id]
+            await self.pubsub_client.unsubscribe(chanel_id)
+
+    async def _pubsub_data_reader(self, pubsub_subscriber):
+        """
+        Reads and broadcasts messages received from Redis PubSub.
+
+        Args:
+            pubsub_subscriber (aioredis.ChannelSubscribe): PubSub object for the subscribed channel.
+        """
+        while True:
+            message = await pubsub_subscriber.get_message(ignore_subscribe_messages=True)
+            if message is not None:
+                chanel_id = message['channel'].decode('utf-8')
+                all_sockets = self.chanels[chanel_id]
+                for socket in all_sockets:
+                    data = message['data'].decode('utf-8')
+                    await socket.send_text(data)
